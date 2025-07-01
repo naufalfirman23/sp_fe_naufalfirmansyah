3358a91208dca54267d7cf1421a7173ec4a4a9d9
Naufal Firmansyah <121421066+naufalfirman23@users.noreply.github.com>
2025-07-01 15:53:48 +0700

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,7 +5,7 @@ export const useApi = () => {
   const token = useAuth.getState().token;
 
   return axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: 'https://spbenaufalfirmansyah-production.up.railway.app/api',
     headers: {
       Authorization: `Bearer ${token}`,
     },
